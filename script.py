f83c2d50c580676ad35cd7781ee7e3461962768b
Redzli <32372904+Redzli@users.noreply.github.com>
2019-01-02 23:51:19 -0500

    Add files via upload
    This file overwrites the previous script.py as the final version

--- script.py
+++ script.py
@@ -6,13 +6,26 @@ Created on Mon Dec 31 00:26:50 2018
 @author: lizhengyuan
 """
 
+# This file has to be run in the same directory with data.csv
 # How to run this script:
-# By clicking the 'run' button, a graph will be generated
+# type 'python script.py' in terminal
 
 
-
+#from prettytable import PrettyTable
+import numpy as np
 import csv
 import matplotlib.pyplot as plt
+import pandas as pd
+
+#table=PrettyTable()
+
+#table.field_names=["purpose","avg_rate"]
+
+#def dataframe(cols, ind):
+#    """Quickly make a DataFrame"""
+#    data = {c: [str(c) + str(i) for i in ind]
+#            for c in cols}
+#    return pd.DataFrame(data, ind)
 
 with open('data.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')   #open data.csv
@@ -32,18 +45,40 @@ with open('data.csv') as csv_file:
     for key in dic:
         avg_rate=sum(dic[key])/len(dic[key])    #calculate average interest rate
         dic[key]=avg_rate
-        print(key, dic[key])
+
     
-        
-    names = ['credit_card','car','small_business','other','wedding','debt_consolidation','home_improvement',
-             'major_purchase','medical','moving','vacation','house']  #label the x-axis                   
-    
-    y=dic.values()   #values are plotted on y-axis
-    
-    plt.bar(names,y, width=0.7, color=['yellow','blue','green','red','orange','purple','grey','brown'])
-    
+    graph_list=dic.keys() 
+    graph_list.sort()   # alphabetically sort the contents
+    int_list=[]
+    text=[]
+    for graph in graph_list:
+        int_list.append(dic[graph])
+        text.append([graph,'%.3f'%dic[graph]])
+
+    fig = plt.figure(figsize=(23,6))    #create the table
+    ax = plt.subplot2grid((7,14), (0,0), colspan=3)
+    col_label=('purpose','avg_rate')    #specify the headers of table
+    table=ax.table(cellText=text,colLabels=col_label,loc='upper right')
+    ax.axis("off")
+
+    plt.subplot2grid((5,12), (0,4),colspan=9,rowspan=3)
+    left = np.arange(len(dic))
+    height = int_list   #make the avg_int into rows
+
+    color_list=['red','yellow','blue','green','brown','pink','lightblue',
+    'purple']  #specify the colours
+    plt.bar(left,height,color=color_list,align='center')
+
+    #plot the table and graph:
     plt.xlabel('purpose')
-    
     plt.ylabel('mean(int_rate)')
-    
+    plt.xticks(left,graph_list,fontsize=7)
+    plt.yticks(np.arange(0, 20, 2.5),fontsize=6)
+
     plt.show()
+
+
+
+
+
+
